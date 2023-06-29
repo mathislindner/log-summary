fdb84af6f7f9bee7a3edfadc5e7d9cf23b228e99
mlindner <mathis.lindner@gmail.com>
2023-06-29 11:04:58 +0200

    can save to df

--- components/data-preprocessing/preprocess-logs-for-langchain/src/preprocess-logs-for-langchain.py
+++ components/data-preprocessing/preprocess-logs-for-langchain/src/preprocess-logs-for-langchain.py
@@ -2,19 +2,42 @@
 import argparse
 import json
 import os
+import pandas as pd
 
 #options is a list of strings that are the keys of the json file
 def keep_only(json_log, options):
-    for option in options:
-        json_log = json_log[option]
-    return json_log
+    hits = json_log["hits"]["hits"]
+    columns_to_keep = []
+    for hit in hits:
+        source = hit["_source"]
+        keep_only_dict = {}
+        for option in options:
+            keep_only_dict[option] = source[option]
+        columns_to_keep.append(keep_only_dict)
+    df = pd.DataFrame(columns_to_keep)
+    #fix host column
+    try:
+        df["host"] = df["host"].apply(lambda x: x['hostname'])
+    except:
+        pass
+    print(df.head())
+    return df
 
 def create_preprocessed_folder(raw_logs_path):
     preprocessed_logs_path = raw_logs_path.replace("raw", "preprocessed")
-    os.mkdirs(preprocessed_logs_path)
+    os.makedirs(preprocessed_logs_path, exist_ok=True)
     return preprocessed_logs_path
 
-
+def save_json_log_to_df(path_to_json_log):
+    options = ["host", "message", "@timestamp"]
+    #load the json file
+    with open(path_to_json_log) as json_file:
+        json_log = json.load(json_file)
+    #keep only the message, server and time
+    df_keep_only = keep_only(json_log, options)
+    #save the json file in the preprocessed folder
+    preprocessed_df_path = path_to_json_log.replace("raw", "preprocessed").replace(".json", ".csv")
+    df_keep_only.to_csv(preprocessed_df_path, index=False)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -26,17 +49,14 @@ if __name__ == '__main__':
     #create the preprocessed folder
     preprocessed_logs_path = create_preprocessed_folder(args.log_path)
 
-    warning_logs_path = os.path.join(args.log_path, "warning")
-    options = ["_source.host.hostname", "_source.message", "_source.@timestamp"]
-    #load the json file
-    with open(args.json_path) as json_file:
-        json_log = json.load(json_file)
-    #keep only the message, server and time
-    json_log = keep_only(json_log, options)
-    #save the json file in the preprocessed folder
-    preprocessed_json_path = warning_logs_path.replace("raw", "preprocessed")
-    with open(os.path.join(preprocessed_logs_path, preprocessed_json_path), "w") as json_file:
-        json.dump(json_log, json_file)
+    #for each log
+    for log in logs:
+        #save the json log to a dataframe
+        json_log_path = os.path.join(args.log_path, log)
+        save_json_log_to_df(json_log_path)
+        #save the dataframe in the preprocessed folder
 
+
+    
     #chunk json by message
 
