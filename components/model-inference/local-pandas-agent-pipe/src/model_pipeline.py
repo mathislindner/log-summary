126775424342da5a9e8267b8af264a3531617080
mlindner <mathis.lindner@gmail.com>
2023-07-06 15:10:42 +0200

    trying with python agent tool

---
+++ components/model-inference/local-pandas-agent-pipe/src/model_pipeline.py
@@ -0,0 +1,23 @@
+import transformers
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+def get_model_pipeline(model_name):
+    tokenizer = AutoTokenizer.from_pretrained(f"/data/models/{model_name}-tokenizer", device_map="auto", trust_remote_code=True, load_in_8bit=True)
+    model = AutoModelForCausalLM.from_pretrained(f"/data/models/{model_name}-model", device_map="auto", trust_remote_code=True,  load_in_8bit=True, temperature=0.1)
+    ############################################################################################################
+    #https://betterprogramming.pub/creating-my-first-ai-agent-with-vicuna-and-langchain-376ed77160e3
+    ############################################################################################################
+    #"text-generation",
+    pipe = transformers.pipeline(
+        "text2text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        trust_remote_code=True,
+        do_sample = True,
+        max_length=1500,
+        top_k=30,
+        num_return_sequences=1,
+        eos_token_id=tokenizer.eos_token_id,
+        pad_token_id=tokenizer.eos_token_id,
+    )
+    return pipe
